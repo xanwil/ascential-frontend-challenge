e98bd890fe784f1c1c6c5a34d0c3c5dc5511343d
xanwil <97523333+xanwil@users.noreply.github.com>
2025-09-07 15:46:31 +0100

    create event and venue interfaces

---
+++ src/types/favourites.ts
@@ -0,0 +1,17 @@
+export interface FavouriteEvent {
+  id: string;
+  short_title: string;
+  datetime_utc: string;
+  venue: {
+    name_v2: string;
+    display_location: string;
+  };
+}
+
+export interface FavouriteVenue {
+  id: number;
+  name_v2: string;
+  display_location: string;
+  has_upcoming_events: boolean;
+  num_upcoming_events: number;
+}
